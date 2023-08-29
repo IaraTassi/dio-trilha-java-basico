acbec4f1e08c0eb730f398ca573bced3fb3838c7
Iara Tassi <iaratassi@hotmail.com>
2023-08-29 08:58:17 -0300

    Add Javadoc Calculadora Java terminal

---
+++ calculadora/src/Calculadora.java
@@ -0,0 +1,22 @@
+/*CLASSE CALCULADORA criada para exemplo do Java Doc
+ //comando javadoc javadoc -encoding UTF-8 -docencoding ISO-8859-1 -d ../docs src/*.java
+ * serve para calcular dois números 
+ * este cógigo serve para mostrar a importância do Java Doc
+ 
+    @author Iara Tassi
+    @version 1.0
+    @since 28/08/2023
+    */
+public class Calculadora {
+   
+           /*
+        Este método é utilizado para somar dois números inteiros
+        @param numeroUm este é o primeiro parâmetro do método
+        @param numeroDois este é o segundo parâmentro do método
+        @return este é o resultado deste método int e é a soma dos dis números
+         */
+        public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+     
+    }
+}
---
+++ calculadora/src/Comentarios.java
@@ -0,0 +1,19 @@
+public class Comentarios {
+  public static void main(String[] args) {
+    //Olá, este é um comentário em uma única linha
+
+    /*
+     * Olá, 
+     * este comentário mais detalhado ocupando mais linhas
+     */
+  }
+  /**
+   * Estas duas estrelinhas acima é para comentário
+   * a nivel de documentação.
+   * Também vimos que não devemos criar metódos as pressas 
+   * foi mostrado método que tem duas funções váriaveis não 
+   * bem definidas. Devemos seguir as boas práticas sempre, 
+   * com códigos simples e legiveis.
+   */
+ 
+}
